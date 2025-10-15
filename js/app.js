5d1ebd12963c70852c5b8e4ea0d9432f5a37cdff
Valentina Iramain <valentinairamain2020@gmail.com>
2025-10-15 10:10:06 -0300

    se realizan funciones para pausar y reiniciar el temporizador

--- js/app.js
+++ js/app.js
@@ -4,30 +4,43 @@ function iniciar(e) {
     const h = parseInt(inputHoras.value) || 0;
     const m = parseInt(inputMinutos.value) || 0;
     const s = parseInt(inputSegundos.value) || 0;
-    tiempoTotalSeg = h * 3600 + m * 60 + s;
-    
+    tiempoTotalSeg = h * 3600 + m * 60 + s; // calcula la cantidad de veces que correra el temporizador
+
     if (tiempoTotalSeg >= 0) {
         clearInterval(intervalo)
-        intervalo = setInterval(temporizador, 1000); // Actualizar cada segundo
+        intervalo = setInterval(temporizador, 1000); // ejecuta temporizador() cada segundo
+        console.log(intervalo)
     }
 }
 
 function temporizador() {
     if (tiempoTotalSeg < 0) {
-        clearInterval(intervalo);     // Detiene el contador
+        clearInterval(intervalo);     // Detiene el setInterval
         console.log('¡Tiempo terminado!');
+        formulario.reset()
         return;
     }
 
+    // arma como va mostrando el reloj en pantalla con cada segundo
     const horas = Math.floor(tiempoTotalSeg / 3600);
     const minutos = Math.floor((tiempoTotalSeg % 3600) / 60);
     const segundos = tiempoTotalSeg % 60;
-    contador.textContent =
-        `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+    contador.textContent = `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundos).padStart(2, '0')}`;
+
     tiempoTotalSeg--
 }
 
+function pausar() {
+    clearInterval(intervalo);
+}
 
+function reIniciar() {
+    // si el tiempo es mayor a 0, vuelve a correr
+    if (tiempoTotalSeg > 0) {
+        clearInterval(intervalo);
+        intervalo = setInterval(temporizador, 1000);
+    }
+}
 
 
 // *********  DOM *************
@@ -37,12 +50,15 @@ const inputHoras = document.querySelector('#inputHoras')
 const inputMinutos = document.querySelector('#inputMinutos')
 const inputSegundos = document.querySelector('#inputSegundos')
 const contador = document.querySelector('h2');
-const btnIniciar = document.querySelector('.btn-warning');
-// const btnPausar = document.querySelector('btnPausar');
-const btnReiniciar = document.querySelector('.btn-secondary');
+const btnIniciar = document.querySelector('.colorBtnIniciar');
+const btnPausar = document.querySelector('.colorBtnPausar');
+const btnReiniciar = document.querySelector('.colorBtnReIniciar');
 
 let tiempoTotalSeg = 0
-let intervalo = 0
+let intervalo = 0 //ID para detener el setInterval con clearInterval
+contador.textContent = '00:00:00'
 //eventos
 formulario.addEventListener('submit', iniciar)
+btnPausar.addEventListener('click', pausar)
+btnReiniciar.addEventListener('click', reIniciar)
 
